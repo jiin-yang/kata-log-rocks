7ac22f570e8ab03b873d4bef94372fccfef8bb30
jiin-yang <jinyang.sv.69@gmail.com>
2021-10-30 15:07:33 +0300

    add 'Turn Off' test

--- 2-christmas-lights/christmas-lights_test.go
+++ 2-christmas-lights/christmas-lights_test.go
@@ -2,17 +2,35 @@ package main
 
 import "testing"
 
-func TestChristmasLights(t *testing.T)  {
+func TestChristmasLights(t *testing.T){
+
+	//turn on
+	//turn off
+	//toggle
+	//multiple instruction
 
 	x1 := 0
 	y1 := 0
 	x2 := 4
 	y2 := 4
-	
-	got := TurnOn(x1, y1, x2, y2)
-	want := 25
 
-	if got != want{
-		t.Errorf("want '%d' but got '%d' ", want, got)
-	}
+	t.Run("turn on lights", func(t *testing.T) {
+
+		got := TurnOn(x1, y1, x2, y2)
+		want := 25
+
+		if got != want{
+			t.Errorf("want '%d' but got '%d' ", want, got)
+		}
+	})
+
+	t.Run("turn off lights", func(t *testing.T) {
+
+		got := TurnOff(x1, y1, x2, y2)
+		want := 0
+
+		if got != want{
+			t.Errorf("want '%d' but got '%d' ", want, got)
+		}
+	})
 }
