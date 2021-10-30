8af0dbae75c5bfa8d9a21b2525228f17598664a8
jiin-yang <jinyang.sv.69@gmail.com>
2021-10-30 15:08:31 +0300

    passed 'Turn Off' test

--- 2-christmas-lights/christmas-lights.go
+++ 2-christmas-lights/christmas-lights.go
@@ -16,3 +16,16 @@ func TurnOn(x1, y1, x2, y2 int) int{
 	}
 	return openLights
 }
+
+func TurnOff(x1, y1, x2, y2 int) int{
+
+	for i := x1; i <= x2; i++ {
+		for j := y1; j <= y2; j++ {
+			if lights[i][j] {
+				openLights--
+				lights[i][j] = false
+			}
+		}
+	}
+	return openLights
+}
