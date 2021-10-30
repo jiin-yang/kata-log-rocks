7759c2ea14798214a6b80f1a94d68f2298004619
jiin-yang <jinyang.sv.69@gmail.com>
2021-10-30 15:04:22 +0300

    passed 'Turn On' test

---
+++ 2-christmas-lights/christmas-lights.go
@@ -0,0 +1,18 @@
+package main
+
+const n = 1000
+var lights [n][n]bool
+var openLights = 0
+
+func TurnOn(x1, y1, x2, y2 int) int{
+
+	for i := x1; i <= x2; i++ {
+		for j := y1; j <= y2; j++ {
+			if !lights[i][j] {
+				openLights++
+				lights[i][j] = true
+			}
+		}
+	}
+	return openLights
+}
