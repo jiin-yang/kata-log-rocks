310aba3fd99aca9da7415aa72159fb63dd01037c
jiin-yang <jinyang.sv.69@gmail.com>
2021-10-30 14:36:50 +0300

    coded to pass test

--- 1-fizz-buzz/fizz-buzz.go
+++ 1-fizz-buzz/fizz-buzz.go
@@ -1,1 +1,23 @@
 package main
+
+import "strconv"
+
+func FizzBuzz(n int) []string {
+
+	result := make([]string, n)
+
+	for i := 1; i <= n; i++{
+
+		if i%15==0 {
+			result[i-1] = "fizzBuzz"
+		}else if i%3== 0 {
+			result[i-1] = "fizz"
+		}else if i%5== 0 {
+			result[i-1] = "buzz"
+		}else {
+			result[i-1] = strconv.Itoa(i)
+		}
+	}
+	return result
+}
+
