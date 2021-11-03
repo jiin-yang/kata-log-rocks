891684df9baa683c72482c9318d6a228209c0d9b
jiin-yang <jinyang.sv.69@gmail.com>
2021-11-03 21:11:47 +0300

    added test for move to the filled place

--- 3-tic-tac-toe/tic-tac-toe_test.go
+++ 3-tic-tac-toe/tic-tac-toe_test.go
@@ -14,4 +14,14 @@ func TestGameArea(t *testing.T){
 		}
 	})
 
+	t.Run("move to the same place", func(t *testing.T) {
+		Move(2, "O")
+		got := Area[1]
+		want := "X"
+
+		if got != want {
+			t.Errorf("want '%s' but got '%s' ", want, got)
+		}
+	})
+
 }
